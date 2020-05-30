3cd522533babdfddfba6f9763a12f177e636b08c
Alexey Hohlov <root@amper.me>
2020-05-30 16:55:27 +0300

    Более правильный учет входящих пакетов, удаление старого кода

--- rhex_rc.c
+++ rhex_rc.c
@@ -44,43 +44,9 @@ rc_main(void)
 		return result;
 	}
 
+	uint32_t last_seqno = 0U;
+
 	for (;;) {
-		#if 0
-		packetcounter_ts_now[i] = current_timestamp();
-		if (packetcounter_ts_now[i] - packetcounter_ts_prev[i] > 1500) {
-			packetcounter_ts_prev[i] = current_timestamp();
-			for (i=0; i<num_interfaces; ++i) {
-				packetcounter_last[i] = packetcounter[i];
-				//packetcounter[i] = rx_status->adapter[i].received_packet_cnt;
-				// fprintf(stderr,"counter:%d last:%d   ",packetcounter[i],packetcounter_last[i]);
-				if (packetcounter[i] == packetcounter_last[i]) {
-				//	rx_status->adapter[i].signal_good = 0;
-					// fprintf(stderr,"signal_good[%d]:%d\n",i,rx_status->adapter[i].signal_good);
-				} else {
-				//	rx_status->adapter[i].signal_good = 1;
-					// fprintf(stderr,"signal_good[%d]:%d\n",i,rx_status->adapter[i].signal_good);
-				}
-			}
-		}
-
-		packetcounter_ts_now_rc[i] = current_timestamp();
-		if (packetcounter_ts_now_rc[i] - packetcounter_ts_prev_rc[i] > 220) {
-			packetcounter_ts_prev_rc[i] = current_timestamp();
-			for(i=0; i<num_interfaces; ++i) {
-				packetcounter_last_rc[i] = packetcounter_rc[i];
-				//packetcounter_rc[i] = rx_status_rc->adapter[i].received_packet_cnt;
-				// fprintf(stderr,"counter:%d last:%d   ",packetcounter_rc[i],packetcounter_last_rc[i]);
-				if (packetcounter_rc[i] == packetcounter_last_rc[i]) {
-				//	rx_status_rc->adapter[i].signal_good = 0;
-					// fprintf(stderr,"signal_good[%d]:%d\n",i,rx_status_rc->adapter[i].signal_good);
-				} else {
-				//	rx_status_rc->adapter[i].signal_good = 1;
-					// fprintf(stderr,"signal_good[%d]:%d\n",i,rx_status_rc->adapter[i].signal_good);
-				}
-			}
-		}
-		#endif
-
 		struct timeval to;
 		to.tv_sec = 0;
 		to.tv_usec = 1e5; // 100ms timeout
@@ -91,52 +57,63 @@ rc_main(void)
 			FD_SET(rc_rx.iface[i].selectable_fd, &readset);
 		}
 
+		int8_t best_dbm = -127;
+
 		int n = select(30, &readset, NULL, NULL, &to); // TODO: check what the 30 does exactly
-		if (n == 0) {
-			continue;
-		}
 
-		for (i = 0; i < rc_rx.count; i++) {
-			if (FD_ISSET(rc_rx.iface[i].selectable_fd, &readset)) {
-				wfb_rx_packet_t rx_data = {0,};
-				//process_packet(interfaces + i, i,serialport);
-				if (wfb_rx_packet(&rc_rx.iface[i], &rx_data)) {
-					struct _r {
-						uint32_t seqno;
-						int16_t rc1;
-						int16_t rc2;
-						int16_t rc3;
-						int16_t rc4;
-					};
+		if (n > 0) {
+			for (i = 0; i < rc_rx.count; i++) {
+				if (FD_ISSET(rc_rx.iface[i].selectable_fd, &readset)) {
+					wfb_rx_packet_t rx_data = {0,};
 
-					union {
-						struct _r *r;
-						uint8_t *u8;
-					} r;
+					if (wfb_rx_packet(&rc_rx.iface[i], &rx_data)) {
+						struct _r {
+							uint32_t seqno;
+							int16_t rc1;
+							int16_t rc2;
+							int16_t rc3;
+							int16_t rc4;
+						};
 
-					r.u8 = rx_data.data;
+						union {
+							struct _r *r;
+							uint8_t *u8;
+						} r;
 
-					r.r->rc2 -= 1500;
-					r.r->rc3 -= 1500;
+						r.u8 = rx_data.data;
 
-					//printf("recv rc -%idbm %u, rc2=%i, rc3=%i\n", rx_data.dbm, r.r->seqno, r.r->rc2, r.r->rc3);
+						if ((uint32_t)(r.r->seqno - last_seqno) < (UINT32_MAX / 2U)) {
+							r.r->rc2 -= 1500;
+							r.r->rc3 -= 1500;
 
-					rc_data.speed = (float)r.r->rc2 / 500.0f;
-					shm_map_write(&rc_shm, &rc_data, sizeof(rc_data));
+							if ((uint32_t)(r.r->seqno - last_seqno) > 1U) {
+								rc_status.lost_packet_cnt += (uint32_t)(r.r->seqno - last_seqno);
+							}
 
-					rc_status.received_packet_cnt++;
-					rc_status.signal_dbm = rx_data.dbm;
+							//printf("recv rc -%idbm %u, rc2=%i, rc3=%i\n", rx_data.dbm, r.r->seqno, r.r->rc2, r.r->rc3);
 
-					shm_map_write(&rc_status_shm, &rc_status, sizeof(rc_status));
+							rc_data.speed = (float)r.r->rc2 / 500.0f;
+							shm_map_write(&rc_shm, &rc_data, sizeof(rc_data));
 
-					/*int p;
-					for (p = 0; p < rx_data.bytes; p++) {
-						printf("%02X ", rx_data.data[p]);
+							rc_status.received_packet_cnt++;
+						}
+
+						if (rx_data.dbm > best_dbm) {
+							best_dbm = rx_data.dbm;
+						}
+
+						/*int p;
+						for (p = 0; p < rx_data.bytes; p++) {
+							printf("%02X ", rx_data.data[p]);
+						}
+						puts("\n");*/
 					}
-					puts("\n");*/
 				}
 			}
 		}
+
+		rc_status.signal_dbm = best_dbm;
+		shm_map_write(&rc_status_shm, &rc_status, sizeof(rc_status));
 	}
 
 	return result;
