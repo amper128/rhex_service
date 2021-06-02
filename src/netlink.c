317859304e3172f572ba93b7db5fae841b3a7ef8
Alexey Hohlov <root@amper.me>
2021-06-02 22:50:11 +0300

    для wlan нужно ARPHRD_IEEE80211_RADIOTAP

--- src/netlink.c
+++ src/netlink.c
@@ -266,7 +266,7 @@ nl_get_eth_list(if_desc_t if_list[])
 int
 nl_get_wifi_list(if_desc_t if_list[])
 {
-	return nl_link_list(if_list, ARPHRD_IEEE80211);
+	return nl_link_list(if_list, ARPHRD_IEEE80211_RADIOTAP);
 }
 
 int
